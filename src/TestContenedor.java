d5b8246a9d317ee7976d6158cf896fd2f31fbaf6
Markitoh <Markitohh20@gmail.com>
2019-06-17 16:24:40 -0400

    First Commit

---
+++ src/TestContenedor.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestContenedor {
+
+	@Test
+	public void carroTest() {
+		
+		Contenedor car = new Contenedor();
+		String da = Contenedor.carro();
+		
+		
+		Assert.assertEquals(enlace, actualURL);
+		
+		
+	}
+	
+	
+	
+	
+	
+}
